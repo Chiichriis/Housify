7ccd9e1cbaa187ba642ed178cab16546570efbab
chiichriis <chichris28@gmail.com>
2022-05-04 15:47:59 -0700

    Update index.js

--- Housify/index.js
+++ Housify/index.js
@@ -24,6 +24,29 @@ const toggle = document.querySelector('.toggle');
 
 navigation.forEach(Element => {
   Element.addEventListener('click', e => toggle.checked = false);
-});
+});   
+
+//On Scroll Animation for the Grid Elements
+window.onload = function(){
+  const gridEl = document.querySelectorAll('.gridEl');
+
+  window.addEventListener('scroll', scrollEffect);
+  function scrollEffect () {
+    const trigger = window.innerHeight/6 * 4; //this gets the simplified window height
+    gridEl.forEach(EFFECT => {
+      const gridTop = EFFECT.getBoundingClientRect().top; //gets the height of each of the div
+      if(gridTop <= trigger) {
+        EFFECT.style.opacity = '1';
+        EFFECT.style.transform = 'translateX(0px)';
+        EFFECT.style.transition = '.5s ease-in-out';
+      }
+      else{
+        EFFECT.style.opacity = '0';
+        EFFECT.style.transform = 'translateX(-50px)';
+      }
+    });
+   
+  }
+}
 
 
